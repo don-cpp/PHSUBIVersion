8c78ff63b42d752ddf1878a18035886d3812d2cc
crusaderdonball <danielhoful@gmail.com>
2019-04-05 16:56:06 -0500

    Testing connection with users.

--- app/src/main/java/com/example/phsubiversion/MainActivity.java
+++ app/src/main/java/com/example/phsubiversion/MainActivity.java
@@ -10,4 +10,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //This is a test
+
+
 }
