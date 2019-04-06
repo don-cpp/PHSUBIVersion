9469e67d8df6076dfe97c451edbd092eb3e8b6a4
crusaderdonball <danielhoful@gmail.com>
2019-04-05 20:42:42 -0500

    Added activites, updated names.
    'Login', 'Menu', 'Host Game', and 'Find Game' XML/Java files have been created.

---
+++ app/src/main/java/com/example/phsubiversion/Login.java
@@ -0,0 +1,15 @@
+package com.example.phsubiversion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+
+}
