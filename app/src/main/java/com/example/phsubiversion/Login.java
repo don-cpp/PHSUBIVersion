99f63e5dcdf220f07c91fca3549f1b6377882480
Knowles,Andrew <andrewdale166@gmail.com>
2019-04-07 18:38:04 -0500

    Merge remote-tracking branch 'remotes/origin/master' into Andrew
    # Conflicts:
    #	app/src/main/res/layout/activity_sign_up_dialog.xml

--- app/src/main/java/com/example/phsubiversion/Login.java
+++ app/src/main/java/com/example/phsubiversion/Login.java
@@ -3,9 +3,11 @@ package com.example.phsubiversion;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -16,7 +18,9 @@ import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
 
-    Button SUDialog;
+    Button SUDialog, Login;
+    EditText username, password;
+    DataSnapshot db;
 
     private final FirebaseDatabase database = FirebaseDatabase.getInstance();
     private String uN = "123";
@@ -24,10 +28,30 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        DatabaseReference ref = database.getReference("Persons");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                db = dataSnapshot;
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
         setContentView(R.layout.login_activity);
-
-        SUDialog = (Button) findViewById(R.id.sign_up_button);
-
+        username = findViewById(R.id.username_input);
+        password = findViewById(R.id.password_input);
+        SUDialog = findViewById(R.id.sign_up_button);
+        Login = findViewById(R.id.Login);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validUser(username.getText().toString(), password.getText().toString());
+            }
+        });
         SUDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,37 +60,25 @@ public class Login extends AppCompatActivity {
             }
         });
 
-        validUser(uN, pw);
-
     }
 
 
     public void validUser (String userName, String password)
     {
-        DatabaseReference ref = database.getReference("Persons");
-        ref.addValueEventListener(new ValueEventListener() {
-
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String vpw = dataSnapshot.child(uN + "/password").getValue().toString();
-                if(vpw.equals(pw))
-                {
-
-                }
-                else
-                {
-                    Context context = getApplicationContext();
-                    CharSequence text = "Invalid login info";
-                    int duration = Toast.LENGTH_SHORT;
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-            }
-        });
+        DataSnapshot user = db.child(userName);
+        if(user.child("password").getValue().toString().equals(password))
+        {
+            Intent intent = new Intent(getApplicationContext(),Dashboard.class);
+            startActivity(intent);
+        }
+        else
+        {
+            Context context = getApplicationContext();
+            CharSequence text = "Invalid login info";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
     }
 
     public void CreateUser(String userName, String password)
--- app/src/main/java/com/example/phsubiversion/SignUpDialog.java
+++ app/src/main/java/com/example/phsubiversion/SignUpDialog.java
@@ -1,21 +1,32 @@
 package com.example.phsubiversion;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpDialog extends AppCompatActivity {
 
+    Button login;
     private final FirebaseDatabase database = FirebaseDatabase.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        login = findViewById(R.id.FinishSignUpButton);
         setContentView(R.layout.activity_sign_up_dialog);
-        DatabaseReference ref = database.getReference("Persons");
-        Persons user = new Persons(findViewById(R.id.usernameS).toString(), findViewById(R.id.passwordS).toString());
-        ref.child(findViewById(R.id.usernameS).toString()).setValue(user);
-
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseReference ref = database.getReference("Persons");
+                Persons user = new Persons(findViewById(R.id.usernameS).toString(), findViewById(R.id.passwordS).toString());
+                ref.child(findViewById(R.id.usernameS).toString()).setValue(user);
+                Intent intent = new Intent(getApplicationContext(),Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
