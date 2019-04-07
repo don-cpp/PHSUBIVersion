6c91d9db37bb6514a43fe3a02f944cea7d3ed578
Knowles,Andrew <andrewdale166@gmail.com>
2019-04-07 17:47:40 -0500

    Merge remote-tracking branch 'remotes/origin/master' into Andrew
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/phsubiversion/FindGame.java
    #	app/src/main/res/layout/login_activity.xml

--- app/src/main/java/com/example/phsubiversion/Login.java
+++ app/src/main/java/com/example/phsubiversion/Login.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
--- app/src/main/java/com/example/phsubiversion/SignUpDialog.java
+++ app/src/main/java/com/example/phsubiversion/SignUpDialog.java
@@ -1,7 +1,7 @@
 package com.example.phsubiversion;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SignUpDialog extends AppCompatActivity {
 
