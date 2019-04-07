f8ccf79504799f7fce8fc69c1fd2e21cd1da32a6
grhoads <47043928+grhoads@users.noreply.github.com>
2019-04-07 15:41:34 -0500

    ativity host game gui

--- app/src/main/java/com/example/phsubiversion/HostGame.java
+++ app/src/main/java/com/example/phsubiversion/HostGame.java
@@ -19,5 +19,10 @@ public class HostGame extends AppCompatActivity {
         setSupportActionBar(toolbar);
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
+        //Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        //ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+        //       R.array.sports, android.R.layout.simple_spinner_item);
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //spinner.setAdapter(adapter);
     }
 }
