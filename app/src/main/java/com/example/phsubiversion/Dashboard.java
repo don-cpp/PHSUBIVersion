8c8f6fb5573f6d676b4dea6245b18d631228cf39
Knowles,Andrew <andrewdale166@gmail.com>
2019-04-07 19:17:38 -0500

    Merge remote-tracking branch 'remotes/origin/master' into Andrew
    # Conflicts:
    #	app/src/main/res/layout/activity_sign_up_dialog.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/phsubiversion/Dashboard.java
+++ app/src/main/java/com/example/phsubiversion/Dashboard.java
@@ -1,18 +1,38 @@
 package com.example.phsubiversion;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Dashboard extends AppCompatActivity {
 
+    Button FindGame, HostGame;
     private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         mDatabase = FirebaseDatabase.getInstance().getReference();
+        FindGame = findViewById(R.id.FindGameButton);
+        HostGame = findViewById(R.id.HostGameButton);
+        HostGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),HostGame.class);
+                startActivity(intent);
+            }
+        });
+        FindGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),FindGame.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/phsubiversion/SignUpDialog.java
+++ app/src/main/java/com/example/phsubiversion/SignUpDialog.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -12,18 +13,21 @@ import com.google.firebase.database.FirebaseDatabase;
 public class SignUpDialog extends AppCompatActivity {
 
     Button login;
+    EditText username, password;
     private final FirebaseDatabase database = FirebaseDatabase.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        login = findViewById(R.id.FinishSignUpButton);
         setContentView(R.layout.activity_sign_up_dialog);
+        username = findViewById(R.id.usernameS);
+        password = findViewById(R.id.passwordS);
+        login = findViewById(R.id.FinishSignUpButton);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 DatabaseReference ref = database.getReference("Persons");
-                Persons user = new Persons(findViewById(R.id.usernameS).toString(), findViewById(R.id.passwordS).toString());
-                ref.child(findViewById(R.id.usernameS).toString()).setValue(user);
+                Persons user = new Persons(username.getText().toString(), password.getText().toString());
+                ref.child(username.getText().toString()).setValue(user);
                 Intent intent = new Intent(getApplicationContext(),Login.class);
                 startActivity(intent);
             }
