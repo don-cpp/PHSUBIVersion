aba14edc49aac0adaee14df2435ec8cb447ab1d1
Knowles,Andrew <andrewdale166@gmail.com>
2019-04-07 22:56:17 -0500

    Merge remote-tracking branch 'remotes/origin/master' into Andrew
    # Conflicts:
    #	app/src/main/res/layout/activity_sign_up_dialog.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/phsubiversion/FindGame.java
+++ app/src/main/java/com/example/phsubiversion/FindGame.java
@@ -41,8 +41,6 @@ public class FindGame extends AppCompatActivity implements OnMapReadyCallback, G
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_find_game);
-        setContentView(R.layout.activity_find_game);
         DatabaseReference ref = database.getReference("Games");
         ref.addValueEventListener(new ValueEventListener() {
             @Override
--- app/src/main/java/com/example/phsubiversion/HostGame.java
+++ app/src/main/java/com/example/phsubiversion/HostGame.java
@@ -1,13 +1,14 @@
 package com.example.phsubiversion;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -40,6 +41,11 @@ public class HostGame extends AppCompatActivity {
                 DatabaseReference ref = database.getReference("Games");
                 Games game = new Games(address.getText().toString(), strUN, sport.getSelectedItem().toString(), time.getText().toString());
                 ref.child(strUN).setValue(game);
+                Context context = getApplicationContext();
+                CharSequence text = "Game Saved!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
             }
         });
         address = findViewById(R.id.etHostAddress);
